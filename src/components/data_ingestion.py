fa783682fd8d28fd7e7cd3daabf80f6cfb48c118
Priyesh Kumar Kashyap <priyeshkashyap91@gmail.com>
2026-05-02 16:36:18 +0530

    Data Transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,18 +1,23 @@
 import os
 import sys
+
 from src.exception import CustomException
 from src.logger import logging
+
 import pandas as pd
+
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
  
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationconfig
  
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts', "train.csv")
     test_data_path: str=os.path.join('artifacts', "test.csv")
     raw_data_path: str=os.path.join('artifacts', "data.csv")
-
+ 
 class DataIngestion:
     def __init__(self):
         self.ingestion_config=DataIngestionConfig()
@@ -23,6 +28,7 @@ class DataIngestion:
             df = pd.read_csv("notebook\data\stud.csv")
             logging.info("Read the dataset as dataframe")
 
+            ##Create artifacts folder
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
 
             df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
@@ -33,7 +39,7 @@ class DataIngestion:
             train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
             test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
 
-            logging.info("Ingestion of data is completed")
+            logging.info("Ingestion of data is completed")  
 
             return(
                 self.ingestion_config.train_data_path,
@@ -42,7 +48,10 @@ class DataIngestion:
     
         except Exception as e:
             raise CustomException(e, sys)
-        
+
 if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
