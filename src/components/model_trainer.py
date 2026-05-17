4e7218570ca0db6cddadcd48a56526cff08a6d2e
Priyesh Kumar Kashyap <priyeshkashyap91@gmail.com>
2026-05-17 09:57:01 +0530

    Prediction Pipeline

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -51,10 +51,10 @@ class ModelTrainer:
             best_model_score = max(sorted(model_report.values()))
   
             ## To get best model name from dict
-
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
             ]
+
             best_model = models[best_model_name]
 
             if best_model_score<0.6:
